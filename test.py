3f41be02f9c87fa7939ca24c75710232a0a2287b
Zuzanna Podyma <zuzannapodyma@MacBook-Air-Zuzanna.local>
2022-04-02 13:35:14 +0200

    utworzenie konfliktu

--- test.py
+++ test.py
@@ -1,10 +1,16 @@
 print("Hello world")
 
-def dodaj(a,b):
-    wynik=float(a)+float(b)
-    return wynik
+def dodaj(a,b):sd
+    wynik=float(a)+float(b)ds
+    return wyniksd
 
-pierwsza=input()
-druga=input()
+pierwsza=input()adsad
+druga=input()s
 
 print(dodaj(pierwsza,druga))
+
+def odejmij(a,b):
+    wynik=float(a)-float(b)
+    return wynik
+
+print(odejmij(pierwsza,druga))
